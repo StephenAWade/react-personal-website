ece53cb328061cab3ac3fff5f7966d88babebf35
Stephen Wade <swade6@cnm.edu>
2022-09-26 20:13:32 -0600

    fixed navbar

--- frontend/src/ui/Navigation.js
+++ frontend/src/ui/Navigation.js
@@ -4,23 +4,23 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 export function Navigation() {
   return (
     <Container>
-    <Navbar bg="white" expand="md">
+      <Navbar bg="white" expand="md">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="m-auto">
+          <Nav>
             <Nav.Link href="/">Home</Nav.Link>
           </Nav>
-          <Nav className="m-auto">
+          <Nav>
             <Nav.Link href="/#resume">Resume</Nav.Link>
           </Nav>
-          <Nav className="m-auto">
+          <Nav>
             <Nav.Link href="/#map">Map</Nav.Link>
           </Nav>
           {/* <Nav className="m-auto">
             <Nav.Link href="/#contact">Contact</Nav.Link>
           </Nav> */}
         </Navbar.Collapse>
-    </Navbar>
+      </Navbar>
     </Container>
   );
 }
